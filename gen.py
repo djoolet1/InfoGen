73cfbd9ea366709c2927906e792049ff3148aec9
djoolet1 <126728862+djoolet1@users.noreply.github.com>
2023-10-02 15:36:13 +0200

    Add files via upload

---
+++ gen.py
@@ -0,0 +1,33 @@
+import requests
+
+# Unesite svoj Genius API ključ ovde
+GENIUS_API_KEY = '3lX2tNy8PUf4J03fo8gAkSFK0C7iBv70lTYzLp3Z__eU1kfyPECueD71QMvWpW4M'
+
+# Unesite ime izvođača kog želite da pretražujete
+izvodjac = input(" Travis Scott ")
+
+# Postavite URL za pretragu pesama izvođača
+search_url = f'https://api.genius.com/search?q={izvodjac}'
+
+# Postavite zaglavlje sa API ključem
+headers = {
+    'Authorization': f'Bearer {GENIUS_API_KEY}'
+}
+
+# Izvršite GET zahtev ka Genius API-ju
+response = requests.get(search_url, headers=headers)
+
+# Proverite da li je zahtev uspeo
+if response.status_code == 200:
+    data = response.json()
+    hits = data['response']['hits']
+    
+    if len(hits) == 0:
+        print(f"Nema pesama za izvođača: {izvodjac}")
+    else:
+        print(f"Pesme izvođača: {izvodjac}")
+        for hit in hits:
+            title = hit['result']['title']
+            print(title)
+else:
+    print(f"Greška pri pretrazi: {response.status_code}")
